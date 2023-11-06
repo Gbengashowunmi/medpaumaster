7a66dd2cdd82d6152e1f360c460fdc763ff109da
unknown <gbengashowunmi01@gmail.com>
2023-11-06 11:50:23 +0100

    new feature

--- src/component/whatsappchat/Whatsapp.jsx
+++ src/component/whatsappchat/Whatsapp.jsx
@@ -1,18 +1,32 @@
 import { Link } from "react-router-dom";
 import "./whatsapp.scss";
+import { useState } from "react";
 function Whatsapp() {
+    const [showWhatasapp, setShowWhatsapp] =useState(false)
+
+    setTimeout(() => {
+        setShowWhatsapp(true)
+    }, 3000);
   return (
-    <div className="whatsapp">
-      <Link
-        to="//api.whatsapp.com/send?phone=2349085323596&text=Hi+MedPau+International+Limited.+I+want+to+make+enquiries+on..."
-        target="blank"
-        rel="noopener noreferrer"
-      >
-        <p>
-          <img src="/images/whatsapp_logo.png" alt="whatsapplogo" srcset="" />
-        </p>
-      </Link>
-    </div>
+    <>
+      {showWhatasapp && (
+        <div className="whatsapp">
+          <Link
+            to="//api.whatsapp.com/send?phone=2349085323596&text=Hi+MedPau+International+Limited.+I+want+to+make+enquiries+on..."
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            <p>
+              <img
+                src="/images/whatsapp_logo.png"
+                alt="whatsapplogo"
+                srcset=""
+              />
+            </p>
+          </Link>
+        </div>
+      )}
+    </>
   );
 }
 
